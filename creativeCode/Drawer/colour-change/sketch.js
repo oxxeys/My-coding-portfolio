76afdf4265ab77ddd01b441a2f626c5a7c20dd06
oxxeys <oxxeys@gmail.com>
2024-05-30 02:23:33 +0100

    Drawer js

---
+++ creativeCode/Drawer/colour-change/sketch.js
@@ -0,0 +1,64 @@
+let pg
+let verts=[]
+let maxVerts = 1500
+let x=0
+let y=0
+let FrameCount
+
+function setup() {
+  createCanvas(400, 400);
+  pg = createGraphics(400,400)
+  frameRate(60)
+}
+
+function mousePressed(){
+  stroke(1)
+  noFill()
+  ellipse(mouseX,mouseY, 20)
+  x=mouseX
+  y=mouseY
+  FrameCount = frameRate()
+}
+
+function mouseReleased(){
+  stroke(1)
+  noFill()
+  ellipse(mouseX,mouseY, 20)
+  
+  pg.line(x,y,mouseX,mouseY)
+  clear()
+}
+
+function draw() {
+  background(0, 50)
+  if(mouseIsPressed){
+    //let d=dist(pmouseX,pmouseY,mouseX,mouseY)
+    //line(pmouseX,pmouseY,mouseX,mouseY)
+    strokeWeight(1)
+    let ellipseColour=color(150, 100, 100, 100)
+    if (FrameCount > frameRate+20){
+      ellipseColour=color(153, 255, 153)
+    }
+    
+    fill(ellipseColour)
+    noStroke()
+    //ellipse(mouseX,mouseY, 20)
+    //line(x,y,mouseX,mouseY)
+    
+    
+    beginShape()
+    verts.forEach(function(vert){
+      vertex(vert.x, vert.y)
+    })
+    
+    endShape()
+    verts.push({x:mouseX, y: mouseY})
+    
+    if(verts.length>maxVerts){
+      verts.shift()
+    }
+  }
+  pg.background(200)
+  //image(pg, 0, 0)
+  
+}
